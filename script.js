ea2affa4f473663c8dd5fb4695d71be2748fa327
Anthony Bergen <50747592+adbergen@users.noreply.github.com>
2020-07-31 21:23:29 -0400

    fixed time to increment

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ $(document).ready(function () {
 
 
 
-  function date_time() {
-    today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
-    $("#currentDay").append(today);
-    setTimeout(function () { date_time(); }, 1000);
+  function time() {
+    $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'));
   }
-  date_time();
+
+  setInterval(update, 1000);
+
+
 
 
 
